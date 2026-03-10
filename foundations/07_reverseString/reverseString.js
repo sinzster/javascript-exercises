0983aa0572053bdb8eb781f18a1095d7f8174fa8
Abhishek Nage <abhishek.nage@gmail.com>
2026-03-10 12:59:34 +1100

    Complete 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (strValue) {
+  const reverseArray = [];
+  const strArray = strValue.split("");
+  for (let i = strValue.length - 1; i >= 0; i--) {
+    reverseArray.push(strArray[i]);
+  }
+  return reverseArray.join("");
 };
 
 // Do not edit below this line
