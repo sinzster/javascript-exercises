6e8d479b4d429f8dfadc07ab08329df696b9cf5d
Abhishek Nage <abhishek.nage@gmail.com>
2026-03-10 12:49:46 +1100

    Complete 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (strValue, repeat) {
+  if (repeat < 0) {
+    return "ERROR";
+  }
+  let repeatString = "";
+  for (let i = 0; i < repeat; i++) {
+    repeatString += strValue;
+  }
+  return repeatString;
 };
 
 // Do not edit below this line
