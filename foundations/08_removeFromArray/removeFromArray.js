5bd89db5889a467cfdc84cb5e977185ef8b540a2
Abhishek Nage <abhishek.nage@gmail.com>
2026-03-10 13:10:46 +1100

    complete 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...toExclude) {
+  for (item of toExclude) {
+    array = array.filter((val) => val !== item);
+  }
+  return array;
 };
 
 // Do not edit below this line
