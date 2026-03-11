40e387a47d096565cb9d26dfd589368fd3335f79
Abhishek Nage <abhishek.nage@gmail.com>
2026-03-12 01:10:04 +1100

    Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let maxOldest = people.reduce(
+    (oldest, person) => {
+      return getAge(person) >= getAge(oldest) ? person : oldest;
+    },
+    { yearOfDeath: 0, yearOfBirth: 0 },
+  );
+  return maxOldest;
 };
+function getAge(person) {
+  if (person.yearOfDeath === undefined) {
+    person.yearOfDeath = new Date().getFullYear();
+  }
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
