5f80e86acc0dd66b5cba1f46098fdad002bfc50f
Abhishek Nage <abhishek.nage@gmail.com>
2026-03-12 00:33:31 +1100

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (value) {
+  value = parseInt(value);
+  if (value < 0) {
+    return "OOPS";
+  }
+  const fibonacci = [];
+  for (let i = 0; i <= value; i++) {
+    if (i === 0) {
+      fibonacci.push(0);
+    } else if (i === 1) {
+      fibonacci.push(1);
+    } else {
+      fibonacci.push(fibonacci.at(-1) + fibonacci.at(-2));
+    }
+  }
+  console.log(fibonacci);
+  return fibonacci[value];
 };
 
 // Do not edit below this line
