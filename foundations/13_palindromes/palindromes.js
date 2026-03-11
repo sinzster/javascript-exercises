35ca0189df76410c25fad3283ded797d3d4b4841
Abhishek Nage <abhishek.nage@gmail.com>
2026-03-12 00:14:09 +1100

    Complete 13_palidromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (value) {
+  let value1 = value
+    .replaceAll(" ", "")
+    .replaceAll(",", "")
+    .replaceAll(".", "")
+    .replaceAll("!", "")
+    .toLowerCase()
+    .split("");
+  console.log(value1);
+  let value2 = value1.toReversed();
+  for (let i = 0; i < value1.length; i++) {
+    if (value1[i] !== value2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
