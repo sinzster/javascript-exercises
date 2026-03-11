5628f4cdedbfe9fdcd9eabc3a6990bab4da801b2
Abhishek Nage <abhishek.nage@gmail.com>
2026-03-11 23:48:55 +1100

    Complete 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (values) {
+  return values.reduce((sum, item) => (sum += item), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((result, item) => (result *= item), 1);
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (value) {
+  if (value === 0) {
+    return 1;
+  }
+  return value * factorial(value - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +32,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
